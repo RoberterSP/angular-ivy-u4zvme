e240bf2aa2ebc8525746496ee81b2d3eea0fc579
RoberterSP <1970668819@qq.com>
2021-10-15 10:51:45 +0800

    remove code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,19 @@
 import { Component, VERSION } from '@angular/core';
 import { Logger } from './app.service';
-import * as _ from 'lodash'
-import * as  _moment from 'moment'
+import * as _ from 'lodash';
 
 @Component({
   providers: [Logger],
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   constructor(private service: Logger) {
     this.service.log('this is log');
-    let aa = {Id: 2}
-    let bb = { name: 6}
-    const aaa = _moment()
-    console.log(_.merge(aa, bb))
+    let aa = { Id: 2 };
+    let bb = { name: 6 };
+    console.log(_.merge(aa, bb));
   }
 
   color = '';
